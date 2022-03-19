e860e81e53f2beef9ba059f2c9ed0065f2549bbc
pedropk <pedropk@gmail.com>
2022-03-19 14:59:36 -0300

    Commit Inicial

---
+++ client/app/index.js
@@ -0,0 +1,33 @@
+var campos = [
+    document.querySelector('#data'),
+    document.querySelector('#valor'),
+    document.querySelector('#quantidade')
+];
+
+var tbody = document.querySelector('table tbody');
+
+document.querySelector('.form').addEventListener(
+    'submit', 
+    function(event) {
+        // Cria uma nova TR (Linha)
+        var tr = document.createElement('tr');
+
+        campos.forEach(
+            function(campo) {
+                //Cria uma TD (Coluna) sem informações
+                var td = document.createElement('td');
+
+                // Atribui o Valor do Campo à TD
+                td.textContent = campo.value;
+
+                // Adiciona a TD dentro da TR
+                tr.appendChild(td);
+            }
+        );
+        
+        //alert('Here 01!');
+    }
+);
+
+// Verificar o conteúdo do Array
+console.log(campos);
