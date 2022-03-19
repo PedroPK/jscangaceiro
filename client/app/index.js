51f804f6d8d2e80713eb94708f990875583b2e1a
pedropk <pedropk@gmail.com>
2022-03-19 20:25:24 -0300

    Now the Inset button finally adds a Row with Data

--- client/app/index.js
+++ client/app/index.js
@@ -9,21 +9,35 @@ var tbody = document.querySelector('table tbody');
 document.querySelector('.form').addEventListener(
     'submit', 
     function(event) {
-        // Cria uma nova TR (Linha)
+        // Canceling the Form Submission
+        event.preventDefault();
+
+        // Create a new Row (TR)
         var tr = document.createElement('tr');
 
         campos.forEach(
             function(campo) {
-                //Cria uma TD (Coluna) sem informações
+                // Create a nre Column (TD), withou infromation for now
                 var td = document.createElement('td');
 
-                // Atribui o Valor do Campo à TD
+                // Puts a Field Value in the TD
                 td.textContent = campo.value;
 
-                // Adiciona a TD dentro da TR
+                // Adds the TD into the TR
                 tr.appendChild(td);
             }
         );
+
+        // New Column (TD) to store the Negociation Volume
+        var tdVolume = document.createElement('td');
+
+        // The indexes 1 and 2 will store the Quantity and Value, respectively
+        tdVolume.textContent = campos[1].value * campos[2].value;
+
+        // Add the Volume TD into the TR
+        tr.appendChild(tdVolume);
+
+        tbody.appendChild(tr);
     }
 );
 
